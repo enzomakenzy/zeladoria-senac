e4710f7907d5d3c05cbc549cad1cc4ec57ae7af2
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-08-10 15:53:34 -0300

    feat: create initial bottom routes props

---
+++ src/routes/bottom-app.routes.tsx
@@ -0,0 +1,15 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+type BottomAppProps = {
+  home: undefined;
+  cleanRooms: undefined;
+  profile: undefined;
+}
+
+const { Navigator, Screen } = createBottomTabNavigator<BottomAppProps>(); 
+
+export function BottomAppRoutes() {
+  return (
+
+  )
+}
