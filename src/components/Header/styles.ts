4ae575fbf6beb39f33e18fc203c03c9ff1960e85
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-08-11 18:54:11 -0300

    feat: create headerhontainer of header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const HeaderContainer = styled.View`
+  height: 50px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.BLUE};
+`;
