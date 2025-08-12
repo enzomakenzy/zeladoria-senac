58d3b124ed1a54d83de422cdfce27d540317e44e
enzomakenzy <enzomqueiroz@gmail.com>
2025-08-12 11:41:27 -0300

    feat: base structure of header completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,31 @@
-import { HeaderContainer } from "./styles";
+import { Image } from "react-native";
+
+import { HeaderContainer, HiText, NotificationButton, NotificationIcon, StaffName, TextContainer } from "./styles";
+
+import SenacLogoImage from "@assets/senac-logo.png";
 
 export function Header() {
   return (
     <HeaderContainer>
+      <Image 
+        source={SenacLogoImage} 
+        resizeMode="contain"
+        style={{ width: 75, height: 48 }} 
+      />
+
+      <TextContainer>
+        <HiText>
+          Olá,
+        </HiText>
+
+        <StaffName>
+          Enzo Makenzy!
+        </StaffName>
+      </TextContainer>
       
+      <NotificationButton>
+        <NotificationIcon />
+      </NotificationButton>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,43 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import Notification from "@assets/notification.svg";
 
 export const HeaderContainer = styled.View`
-  height: 50px;
   width: 100%;
+  padding: 0px 15px 10px 15px ;
   background-color: ${({ theme }) => theme.COLORS.BLUE};
+  flex-direction: row;
+  align-items: center;
+  gap: 15px;
 `;
+
+export const TextContainer = styled.View`
+  flex-direction: column;
+  padding-top: 10px;
+  flex: 1;
+`;  
+
+export const HiText = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.WHITE.TRANSPARENCE_100};
+    font-family: ${theme.FONTS.REGULAR};
+  `}
+
+  font-size: 17px;
+`
+
+export const StaffName = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.WHITE.TRANSPARENCE_100};
+    font-family: ${theme.FONTS.BOLD};
+  `};
+  
+  font-size: 17px;
+`
+
+export const NotificationButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 1
+})``
+
+export const NotificationIcon = styled(Notification).attrs(({ theme }) => ({
+  fill: theme.COLORS.WHITE.TRANSPARENCE_100
+}))``
