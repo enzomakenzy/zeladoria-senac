04922e8c5fb67ee5ad301bf2d4157fc3e6fead5f
enzomakenzy <enzomqueiroz@gmail.com>
2025-08-15 10:01:48 -0300

    style: adjust padding

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -12,7 +12,7 @@ export const Container = styled(SafeAreaView).attrs({
 export const Main = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.WHITE.TRANSPARENCE_100};
-  padding: 15px;
+  padding: 15px 15px 0px 15px;
 `;
 
 export const ScreenTitle = styled.Text`
