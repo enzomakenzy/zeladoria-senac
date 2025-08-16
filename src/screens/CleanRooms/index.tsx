846632ca473cb20e69a560fe95100530e679377e
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-08-16 20:24:33 -0300

    feat/create modal base style in cleanrooms

--- src/screens/CleanRooms/index.tsx
+++ src/screens/CleanRooms/index.tsx
@@ -1,19 +1,74 @@
 import { useState } from "react";
 
-import { Container, Main, ScreenTitle } from "./styles";
+import { Container, Main, ModalCategoryText, ModalCloseButton, ModalCloseTextButton, ModalContainer, ModalContentContainer, ModalDetailsContainer, ModalInfoText, ModalRoomNameTitle, ScreenTitle } from "./styles";
 
 import { Header } from "@components/Header";
 import { SearchInput } from "@components/SearchInput";
 import { CleanRoomCard } from "@components/CleanRoomCard";
 import { CleanRoomProps } from "@components/CleanRoomCard";
-import { FlatList } from "react-native";
+import { FlatList, Modal, Pressable, Text, View } from "react-native";
 import { cleanRoomsData } from "@utils/dataTest";
 
 export function CleanRooms() {
   const [cleanRooms, setCleanRooms] = useState<CleanRoomProps[]>(cleanRoomsData)
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleTest() {
+    console.log("disparou")
+  }
 
   return (
     <Container>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        navigationBarTranslucent
+        statusBarTranslucent
+      >
+        <ModalContainer>
+          <ModalDetailsContainer>
+            <ModalRoomNameTitle>Laboratório 2</ModalRoomNameTitle>
+
+            <ModalContentContainer>
+                <ModalInfoText>
+                  <ModalCategoryText>Limpado por: </ModalCategoryText>
+                  Enzo Makenzy
+                </ModalInfoText>
+                
+                <ModalInfoText>
+                  <ModalCategoryText>Data e hora da limpeza: </ModalCategoryText>
+                  04/08/2025 às 15:30
+                </ModalInfoText>
+                
+                <ModalInfoText>
+                  <ModalCategoryText>Capacidade: </ModalCategoryText>
+                  15
+                </ModalInfoText>
+
+                <ModalInfoText>
+                  <ModalCategoryText>Localização: </ModalCategoryText>
+                  Bloco A
+                </ModalInfoText>
+                
+                <ModalInfoText areGreen>
+                  <ModalCategoryText>Status: </ModalCategoryText>
+                  Limpa
+                </ModalInfoText>
+
+                <ModalInfoText>
+                  <ModalCategoryText>Observações: </ModalCategoryText>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                </ModalInfoText>
+            </ModalContentContainer>
+
+            <ModalCloseButton onPress={() => setModalVisible(false)}>
+              <ModalCloseTextButton>Voltar</ModalCloseTextButton>
+            </ModalCloseButton>
+          </ModalDetailsContainer>
+        </ModalContainer>
+      </Modal>
+
       <Header />
 
       <Main>
@@ -27,7 +82,12 @@ export function CleanRooms() {
           showsVerticalScrollIndicator={false}
           style={{ marginTop: 20 }}
           renderItem={({ item }) => (
-            <CleanRoomCard roomName={item.roomName} cleanerBy={item.cleanerBy} dateAndTimeOfCleaning={item.dateAndTimeOfCleaning} />
+            <CleanRoomCard 
+              roomName={item.roomName} 
+              cleanerBy={item.cleanerBy} 
+              dateAndTimeOfCleaning={item.dateAndTimeOfCleaning} 
+              onPress={() => setModalVisible(true)}
+            />
           )}
         />
       </Main>
--- src/screens/CleanRooms/styles.ts
+++ src/screens/CleanRooms/styles.ts
@@ -2,6 +2,10 @@ import styled, { css } from "styled-components/native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
+type ColorCleanRoomProp = {
+  areGreen?: boolean;
+}
+
 export const Container = styled(SafeAreaView).attrs({
   edges: ["right", "left", "top"]
 })`
@@ -24,3 +28,64 @@ export const ScreenTitle = styled.Text`
   margin-bottom: 12px;
 `; 
 
+export const ModalContainer = styled.View`
+  flex: 1;
+  background-color: #00000079;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalDetailsContainer = styled.View`
+  width: 90%;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.COLORS.WHITE.TRANSPARENCE_100};
+  border-radius: 6px;
+  gap: 5px;
+`;
+
+export const ModalRoomNameTitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.SEMI_BOLD};
+    color: ${theme.COLORS.BLUE};
+  `}
+  font-size: 26px;
+  text-align: center;
+`;
+
+export const ModalContentContainer = styled.View`
+  gap: 5px;
+`;
+
+export const ModalCategoryText = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.MEDIUM};
+    color: ${theme.COLORS.BLACK.TRANSPARENCE_100};
+  `}  
+  font-size: 16px;
+`
+
+export const ModalInfoText = styled.Text<ColorCleanRoomProp>`
+  ${({ theme, areGreen }) => css`
+    font-family: ${areGreen ? theme.FONTS.MEDIUM : theme.FONTS.REGULAR};
+    color: ${areGreen ? theme.COLORS.GREEN : theme.COLORS.BLACK.TRANSPARENCE_100};
+  `}  
+  font-size: 16px;
+`
+
+export const ModalCloseButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.55
+})`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.BLUE};
+  padding: 5px;
+  border-radius: 6px;
+`
+
+export const ModalCloseTextButton = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${({ theme }) => theme.FONTS.MEDIUM};
+    color: ${theme.COLORS.WHITE.TRANSPARENCE_100};
+  `}
+  font-size: 16px;
+  text-align: center;
+`;
