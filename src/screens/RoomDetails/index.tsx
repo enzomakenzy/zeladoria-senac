d1dcc1dad356722418f1e55d25021f42101c0ccb
enzomakenzy <enzomqueiroz@gmail.com>
2025-08-20 08:24:06 -0300

    feat: create detailsroom screen

--- src/screens/RoomDetails/index.tsx
+++ src/screens/RoomDetails/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@components/Header";
-import { Container, Main } from "./styles";
+import { Container, InfoRoomContainer, InfoRoomText, Line, Main, RoomNameText, ScreenTitle, StatusRoomContainer, StatusRoomText } from "./styles";
+import { LargeButton } from "@components/LargeButton";
 
 export function RoomDetails() {
   return (
@@ -7,7 +8,49 @@ export function RoomDetails() {
       <Header name="Enzo Makenzy" />
 
       <Main>
-        
+        <ScreenTitle>
+          Detalhes da Sala
+        </ScreenTitle>
+
+        <Line />
+
+        <RoomNameText>
+          Laboratório 3
+        </RoomNameText>
+
+        <InfoRoomContainer>
+          <InfoRoomText>
+            <InfoRoomText textStyle="medium">Capacidade: </InfoRoomText>
+            30
+          </InfoRoomText>
+
+          <InfoRoomText>
+            <InfoRoomText textStyle="medium">Status da limpeza: </InfoRoomText>
+            30
+          </InfoRoomText>
+
+          <InfoRoomText>
+            <InfoRoomText textStyle="medium">Localização: </InfoRoomText>
+            Bloco A
+          </InfoRoomText>
+
+          <InfoRoomText>
+            <InfoRoomText textStyle="medium">Última limpeza: </InfoRoomText>
+            02/08/2025 às 12:00
+          </InfoRoomText>
+
+          <InfoRoomText>
+            <InfoRoomText textStyle="medium">Último funcionário a limpar: </InfoRoomText>
+            Enzo Makenzy
+          </InfoRoomText>
+
+          <InfoRoomText>
+            <InfoRoomText textStyle="medium">Descrição: </InfoRoomText>
+             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an.
+          </InfoRoomText>
+        </InfoRoomContainer>
+
+        <LargeButton primary textButton="Marcar sala como limpa"  />
       </Main>
     </Container>
   )
--- src/screens/RoomDetails/styles.ts
+++ src/screens/RoomDetails/styles.ts
@@ -1,6 +1,12 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
+import Circle from "@assets/circle.svg";
+
+type RoomProps = {
+  status?: boolean;
+  textStyle?: "regular" | "medium";
+}
 
 export const Container = styled(SafeAreaView).attrs({
   edges: ["right", "left", "top"]
@@ -12,4 +18,64 @@ export const Container = styled(SafeAreaView).attrs({
 export const Main = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.WHITE.TRANSPARENCE_100};
+  padding: 15px;
+`;
+
+export const ScreenTitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.SEMI_BOLD};
+    color: ${theme.COLORS.BLACK.TRANSPARENCE_100};
+  `}
+  font-size: 25px;
+  margin-bottom: 12px;
+`; 
+
+export const Line = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.COLORS.BLACK.TRANSPARENCE_6};
+`;
+
+export const RoomNameText = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.MEDIUM};
+    color: ${theme.COLORS.BLUE};
+  `};
+  font-size: 22px;
+  margin: 18px 0px 10px; 
+`;
+
+export const InfoRoomContainer = styled.View`
+  gap: 8px;
+  margin-bottom: 22px;
+`;
+
+export const ItemInfoContainer = styled.View`
+  flex-direction: row;
+  gap: 7px;
+`;
+
+export const InfoRoomText = styled.Text<RoomProps>`
+  ${({ theme, textStyle = "regular" }) => css`
+    font-family: ${textStyle === "regular" ? theme.FONTS.REGULAR : theme.FONTS.MEDIUM};
+    color: ${theme.COLORS.BLACK.TRANSPARENCE_100};
+  `}
+  font-size: 18px;
+`;
+
+export const StatusRoomContainer = styled.View`
+  flex-direction: row;
+  gap: 5px;
+`;
+
+export const StatusRoomText = styled.Text<RoomProps>`
+  ${({ theme, status }) => css`
+    font-family: ${theme.FONTS.REGULAR};
+    color: ${status ? theme.COLORS.GREEN : theme.COLORS.ORANGE};
+  `}
+  font-size: 16px;
+`
+
+export const StatusRoomIcon = styled(Circle)<RoomProps>`
+  background-color: ${({ theme, status }) => status ? theme.COLORS.GREEN : theme.COLORS.ORANGE};
 `;
