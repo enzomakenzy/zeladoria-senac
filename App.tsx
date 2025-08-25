adec2f7c840001f5bcac085686cd19bfc1c9f9b0
enzomakenzy <enzomqueiroz@gmail.com>
2025-08-25 13:05:15 -0300

    chore: starting to applying contexts

--- App.tsx
+++ App.tsx
@@ -5,17 +5,23 @@ import { ThemeProvider } from "styled-components/native";
 
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold } from "@expo-google-fonts/poppins";
 import { Loading } from "@components/Loading";
+import { AuthContext } from "@contexts/AuthController";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold });
 
   return (
-    <ThemeProvider theme={MainTheme}>
-      { fontsLoaded ?
-        <Routes />
-        :
-        <Loading />
-      }
-    </ThemeProvider>
+    <AuthContext.Provider value={{
+      id: "1",
+      name: "Enzo Makenzy"
+    }}>
+      <ThemeProvider theme={MainTheme}>
+        { fontsLoaded ?
+          <Routes />
+          :
+          <Loading />
+        }
+      </ThemeProvider>
+    </AuthContext.Provider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { BottomAppRoutes } from "./bottom-app.routes";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import * as NavigationBar from 'expo-navigation-bar';
+import { AuthContext } from "@contexts/AuthController";
 
 export function Routes() {
+  const contextData = useContext(AuthContext);
+
+  console.log("Usuário Logado => ", contextData);
+
   useEffect(() => {
     NavigationBar.setButtonStyleAsync('light');
   }, []);
