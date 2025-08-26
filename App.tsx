38adc773f90704177b586da0517ac266cacf8a99
enzomakenzy <enzomqueiroz@gmail.com>
2025-08-26 08:28:33 -0300

    fix: refactoring authcontextprovider

--- App.tsx
+++ App.tsx
@@ -5,23 +5,16 @@ import { ThemeProvider } from "styled-components/native";
 
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold } from "@expo-google-fonts/poppins";
 import { Loading } from "@components/Loading";
-import { AuthContext } from "@contexts/AuthController";
+import { AuthContextProvider } from "@contexts/AuthController";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold });
 
   return (
-    <AuthContext.Provider value={{
-      id: "1",
-      name: "Enzo Makenzy"
-    }}>
-      <ThemeProvider theme={MainTheme}>
-        { fontsLoaded ?
-          <Routes />
-          :
-          <Loading />
-        }
-      </ThemeProvider>
-    </AuthContext.Provider>
+    <ThemeProvider theme={MainTheme}>
+      <AuthContextProvider>
+        { fontsLoaded ? <Routes /> : <Loading /> }
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 }
--- src/contexts/AuthController.tsx
+++ src/contexts/AuthController.tsx
@@ -1,3 +1,28 @@
-import { createContext } from "react";
+import { UserDTO } from "@dtos/UserDTO";
+import { createContext, ReactNode } from "react";
 
-export const AuthContext = createContext({})
+export type AuthContextDataProps = {
+  user: UserDTO;
+}
+
+export type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider value={{
+      user: {
+        id: "1",
+        username: "senac",
+        email: "",
+        is_staff: true,
+        is_superuser: true
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
