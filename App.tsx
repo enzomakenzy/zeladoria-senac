5ba00af9bafb6f12e7efa00f685157db8620cd07
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-09-01 06:58:35 -0300

    fix: altered input and home structure

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { Routes } from "@routes/index";
 import MainTheme from "@themes/MainTheme";
 import { ThemeProvider } from "styled-components/native";
 
-import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold } from "@expo-google-fonts/poppins";
+import { useFonts, Quicksand_700Bold, Quicksand_600SemiBold, Quicksand_500Medium, Quicksand_400Regular } from "@expo-google-fonts/quicksand";
 import { Loading } from "@components/Loading";
 import { AuthContextProvider } from "@contexts/AuthController";
 import Toast from "react-native-toast-message";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold });
+  const [fontsLoaded] = useFonts({ Quicksand_700Bold, Quicksand_600SemiBold, Quicksand_500Medium, Quicksand_400Regular });
 
   return (
     <ThemeProvider theme={MainTheme}>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -13,15 +13,15 @@ export const Container = styled(SafeAreaView).attrs({
 export const Main = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.WHITE.TRANSPARENCE_100};
-  padding: 15px 15px 0px 15px;
+  padding: 8px 10px 0px 10px;
 `;
 
 export const ScreenTitle = styled.Text`
   ${({ theme }) => css`
-    font-family: ${theme.FONTS.SEMI_BOLD};
+    font-family: ${theme.FONTS.BOLD};
     color: ${theme.COLORS.BLACK.TRANSPARENCE_100};
   `}
-  font-size: 25px;
+  font-size: 22px;
   margin-bottom: 12px;
 `; 
 
