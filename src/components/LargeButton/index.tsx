f72901469253fb14aa1f0569e9f61ce9bff95e05
enzomakenzy <enzomqueiroz@gmail.com>
2025-10-08 10:00:09 -0300

    feat: implemented new styles buton props

--- src/components/LargeButton/index.tsx
+++ src/components/LargeButton/index.tsx
@@ -1,26 +1,40 @@
+import { FC, Fragment } from "react";
 import { ActivityIndicator, TouchableOpacityProps } from "react-native";
 
 import { Button, ButtonText } from "./styles";
 
 import { ButtonColorProp } from "./styles";
 import { useTheme } from "styled-components/native";
+import { SvgProps } from "react-native-svg";
 
 type Props = ButtonColorProp & TouchableOpacityProps & {
   textButton: string;
   isLoading?: boolean;
+  status?: "active" | "inactive";
+  Icon?: FC<SvgProps>;
 }
 
-export function LargeButton({ textButton, primary = "blue", isLoading, ...rest }: Props) {
+export function LargeButton({ textButton, primary = "blue", isLoading, status, onPress, Icon, ...rest }: Props) {
   const theme = useTheme();
 
   return (
-    <Button primary={primary} {...rest}>
+    <Button 
+      primary={primary} 
+      status={status} 
+      disabled={status === "inactive" ? true : false} 
+      {...rest}>
       { isLoading ? 
         <ActivityIndicator color={theme.COLORS.WHITE[100]} />
       :
-        <ButtonText>
-          {textButton}
-        </ButtonText>
+        <Fragment>
+          <ButtonText primary={primary} status={status}>
+            {textButton}
+          </ButtonText>
+
+          { Icon &&
+            <Icon height={14} width={14} />
+          }
+        </Fragment>
       }
     </Button>
   );
--- src/components/LargeButton/styles.ts
+++ src/components/LargeButton/styles.ts
@@ -2,24 +2,40 @@ import styled, { css } from "styled-components/native";
 
 export type ButtonColorProp = {
   primary?: "blue" | "red" | "orange";
+  status?: "active" | "inactive";
 }
 
 export const Button = styled.TouchableOpacity.attrs({
   activeOpacity: 0.6
 })<ButtonColorProp>`
-  padding: 9px;
+  padding: 8px;
   border-radius: 6px;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme, primary }) => (primary === "blue" ? theme.COLORS.BLUE[200] 
-    : 
-    primary === "red" ? theme.COLORS.RED : theme.COLORS.ORANGE.MAIN
-  )};
+  border-width: 1px;
+  ${({ theme, primary, status }) => css`
+    border-color: ${
+      status === "inactive" ? theme.COLORS.GRAY[100]
+      : primary === "blue" ? theme.COLORS.BLUE[200] 
+      : primary === "red" ? theme.COLORS.RED 
+      : theme.COLORS.ORANGE.MAIN
+    };
+    background-color: ${
+      status === "inactive" ? theme.COLORS.GRAY[100]
+      : primary === "blue" ? theme.COLORS.BLUE[200] 
+      : primary === "red" ? theme.COLORS.WHITE[100] 
+      : theme.COLORS.ORANGE.MAIN
+    };
+  `}
 `;
 
-export const ButtonText = styled.Text`
-  ${({ theme }) => css`
-    color: ${theme.COLORS.WHITE[100]};
+export const ButtonText = styled.Text<ButtonColorProp>`
+  ${({ theme, primary, status }) => css`
+    color: ${
+      status === "inactive" ? theme.COLORS.GRAY[600] 
+      : primary === "red" ? theme.COLORS.RED 
+      : theme.COLORS.WHITE[100]
+    };
     font-family: ${theme.FONTS.REGULAR};
   `}
   font-size: 16px;
