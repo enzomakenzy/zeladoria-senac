d7bddd2693f207286f69be9617e6c9001ec4a6e0
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-10-08 22:01:59 -0300

    feat: created startcleandto

---
+++ src/dtos/StartCleanDTO.ts
@@ -0,0 +1,9 @@
+export interface StartCleanDTO {
+  id: string;
+  sala: string;
+  sala_nome: string;
+  data_hora_fim: string | null;
+  funcionario_responsavel: "zeladoria1",
+  observacoes: string | null;
+  fotos: string[];
+}
